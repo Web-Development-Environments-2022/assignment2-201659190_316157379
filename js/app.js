741162b1cff942f69ef490e1dfed597b1dcd123a
inbarzakaem <inbarza@post.bgu.ac.il>
2022-05-10 20:01:06 +0300

    updte About  section

--- js/app.js
+++ js/app.js
@@ -341,11 +341,37 @@ function LeftButton()
 
 
 /////////////////////// about section ///////////////////////////
-function About()
-{
+// function About()
+// {
+// 		$("#about_menu").dialog({
+// 			model: true,
+// 			// autoOpen: false,
+// 			title: "test",
+// 			hight: 600,
+// 			width: 800,
+// 			draggable: false,
+// 			resizable: false,
+// 			closeOnEscape: true,
+// 			show: 
+// 			{
+// 			effect: "blind",
+// 			duration: 500
+// 			},
+// 			hide: 
+// 			{
+// 			effect: "explode",
+// 			duration: 700
+// 			},
+// 			clickOutside: false,
+// 			// clickOutsideTrigger: ""
+// 		});
+
+// }
+function About() {
+
 		$("#about_menu").dialog({
 			model: true,
-			// autoOpen: false,
+			//autoOpen: false,
 			title: "test",
 			hight: 600,
 			width: 800,
@@ -357,16 +383,54 @@ function About()
 			effect: "blind",
 			duration: 500
 			},
+			
 			hide: 
 			{
 			effect: "explode",
 			duration: 700
 			},
-			clickOutside: false,
-			// clickOutsideTrigger: ""
+			clickOutside: true,
+			clickOutsideTrigger: ".dialog"
+			
+		});
+		$( ".dialog" ).click(function() {
+			$( "#about_menu" ).dialog( "open" );
 		});
 
+
 }
+$.widget( "ui.dialog", $.ui.dialog, {
+	options: {
+	  clickOutside: true, // Determine if clicking outside the dialog shall close it
+	  clickOutsideTrigger: "#about_menu" // Element (id or class) that triggers the dialog opening 
+	},
+  
+	open: function() {
+	  var clickOutsideTriggerEl = $( this.options.clickOutsideTrigger );
+	  var that = this;
+	  
+	  if (this.options.clickOutside){
+		// Add document wide click handler for the current dialog namespace
+		$(document).on( "click.ui.dialogClickOutside" + that.eventNamespace, function(event){
+		  if ( $(event.target).closest($(clickOutsideTriggerEl)).length == 0 && $(event.target).closest($(that.uiDialog)).length == 0){
+			that.close();
+		  }
+		});
+	  }
+	  
+	  this._super(); // Invoke parent open method
+	},
+	
+	close: function() {
+	  var that = this;
+	  
+	  // Remove document wide click handler for the current dialog
+	  $(document).off( "click.ui.dialogClickOutside" + that.eventNamespace );
+	  
+	  this._super(); // Invoke parent close method 
+	},  
+  
+  });
 ////////////////////////////////////////////////////////////////
 
 
